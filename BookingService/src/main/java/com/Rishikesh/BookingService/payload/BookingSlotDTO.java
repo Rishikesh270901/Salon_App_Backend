a7f73c457916c7481f5e6542c5f8e2abcba4cba9
Rishikesh270901 <rishikeshvangari27@gmail.com>
2026-06-04 16:56:29 +0530

    Implement BookingByDate, SalonReport and BookingSlotDTO

---
+++ BookingService/src/main/java/com/Rishikesh/BookingService/payload/BookingSlotDTO.java
@@ -0,0 +1,13 @@
+package com.Rishikesh.BookingService.payload;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BookingSlotDTO {
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+}
